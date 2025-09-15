55743cf68acff17d0c071194983d75509f21ff32
某人 华 <2825338723@qq.com>
2025-09-15 19:42:05 +0800

    2025-9-15 19:42 Update

---
+++ Functions/PosSelectorLib/PosSelectorLib.cs
@@ -0,0 +1,23 @@
+﻿using Debug_NewLibTest.Utils.PosSelectorLib;
+using System.Drawing;
+
+// PosSelectorLib
+// 坐标选择器
+// Version: 2025-9-15 19:41
+
+namespace PosSelectorLib
+{
+    public class PosSelector
+    {
+        public static Point Show(double opacity = 0.2)
+        {
+            using (var window = new PosSelectorForm())
+            {
+                window.Opacity = opacity;
+                window.ShowDialog();
+            }
+
+            return PosSelectorForm.Pos;
+        }
+    }
+}
